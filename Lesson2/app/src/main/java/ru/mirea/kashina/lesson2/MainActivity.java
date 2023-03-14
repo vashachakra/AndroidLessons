4190e4deae78fd90400ae2d9752b734686f6736a
vashachakra <hey_just_bee@mail.ru>
2023-03-14 20:18:12 +0300

    ActivityLifecycle

--- Lesson2/app/src/main/java/ru/mirea/kashina/lesson2/MainActivity.java
+++ Lesson2/app/src/main/java/ru/mirea/kashina/lesson2/MainActivity.java
@@ -1,14 +1,43 @@
 package ru.mirea.kashina.lesson2;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.util.Log;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG,"onStart()");
+    }
+
+    @Override
+    protected void onRestart() {super.onRestart(); }
+    @Override
+    protected void onResume() {super.onResume(); }
+    @Override
+    protected void onPause() {super.onPause(); }
+    @Override
+    protected void onStop() {super.onStop(); }
+    @Override
+    protected void onDestroy() {super.onDestroy(); }
+
+    protected void onRestoreInstanceState(Bundle saveInstanceState) {
+        super.onRestoreInstanceState(saveInstanceState);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
+
 }
