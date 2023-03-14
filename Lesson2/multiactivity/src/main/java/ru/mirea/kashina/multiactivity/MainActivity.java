7d17ff8456d7ce6f6669ef4ad5a6a39068bf1959
vashachakra <hey_just_bee@mail.ru>
2023-03-14 21:08:15 +0300

    second activity

--- Lesson2/multiactivity/src/main/java/ru/mirea/kashina/multiactivity/MainActivity.java
+++ Lesson2/multiactivity/src/main/java/ru/mirea/kashina/multiactivity/MainActivity.java
@@ -3,7 +3,9 @@ package ru.mirea.kashina.multiactivity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+import android.util.Log;
+import android.view.View;
+import android.content.Intent;
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -11,4 +13,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("MainActivity", "OnPause");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("MainActivity", "OnDestroy");
+    }
+
+
 }
