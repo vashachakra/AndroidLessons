acb6a2c3cd0039f9c2573a4f7fc41afb88e26c49
vashachakra <hey_just_bee@mail.ru>
2023-03-19 17:31:19 +0300

    final IntentFilter

--- Lesson2/intentfilter/src/main/java/ru/mirea/kashina/intentfilter/MainActivity.java
+++ Lesson2/intentfilter/src/main/java/ru/mirea/kashina/intentfilter/MainActivity.java
@@ -3,12 +3,26 @@ package ru.mirea.kashina.intentfilter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+import android.content.Intent;
+import android.net.Uri;
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onBrowserClick(android.view.View view){
+        Intent openLinkIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://priem.mirea.ru/first-degree/list"));
+        startActivity(openLinkIntent);
+    }
+
+    public void onDataTransmission(android.view.View view){
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_SUBJECT, "MIREA");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, "Кашина Анастасия Сергеевна");
+        startActivity(Intent.createChooser(shareIntent, "МОИ ФИО"));
     }
 }
