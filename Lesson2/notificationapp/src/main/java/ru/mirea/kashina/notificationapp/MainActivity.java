750242037d51c71adc4ff80c765abae6c6218a5a
vashachakra <hey_just_bee@mail.ru>
2023-03-22 21:48:26 +0300

    Start Dialog

--- Lesson2/notificationapp/src/main/java/ru/mirea/kashina/notificationapp/MainActivity.java
+++ Lesson2/notificationapp/src/main/java/ru/mirea/kashina/notificationapp/MainActivity.java
@@ -1,14 +1,54 @@
 package ru.mirea.kashina.notificationapp;
-
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    // Идентификатор канала
+    private static final String CHANNEL_ID = "com.mirea.asd.notification.ANDROID";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onClickSendNotification (View view) {
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+
+            return;
+        }
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+
+                        .setContentText("Congratulation!")
+                        .setPriority(NotificationCompat.PRIORITY_HIGH)
+                        .setStyle(new NotificationCompat.BigTextStyle()
+                                .bigText("Much longer text that cannot fit one line..."))
+                .setContentTitle("Mirea");
+        int importance = NotificationManager.IMPORTANCE_DEFAULT;
+
+        NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "Student KashinaAS Notification", importance);
+
+                channel.setDescription("MIREA Channel");
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        notificationManager.createNotificationChannel(channel);
+        notificationManager.notify(1, builder.build());
+    }
+
 }
