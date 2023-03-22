f949f41f6f4c4c429f1fcbebdf7d3a3a2a787bb1
vashachakra <hey_just_bee@mail.ru>
2023-03-22 23:56:12 +0300

    finally ready

--- Lesson2/notificationapp/src/main/java/ru/mirea/kashina/notificationapp/MainActivity.java
+++ Lesson2/notificationapp/src/main/java/ru/mirea/kashina/notificationapp/MainActivity.java
@@ -1,8 +1,11 @@
 package ru.mirea.kashina.notificationapp;
+
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
+import androidx.core.content.ContextCompat;
 
 import android.app.Notification;
 import android.app.NotificationChannel;
@@ -12,43 +15,49 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-    // Идентификатор канала
-    private static final String CHANNEL_ID = "com.mirea.asd.notification.ANDROID";
+import static android.Manifest.permission.POST_NOTIFICATIONS;
 
+public class MainActivity extends AppCompatActivity {
+    private static final String CHANNEL_ID = "com.mirea.KashinaAS.notification.ANDROID";
+    private int IDENTIFICATE_MSG = 0;
+
+    private int PermissionCode = 200;
+    @RequiresApi(api = Build.VERSION_CODES.TIRAMISU)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void onClickSendNotification (View view) {
+        if (ContextCompat.checkSelfPermission(this, POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED) {
 
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+            Log.d(MainActivity.class.getSimpleName().toString(), "Разрешения получены");
 
-            return;
         }
+        else {
+            Log.d(MainActivity.class.getSimpleName(), "Нет разрешений!");
 
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
-
-                        .setContentText("Congratulation!")
-                        .setPriority(NotificationCompat.PRIORITY_HIGH)
-                        .setStyle(new NotificationCompat.BigTextStyle()
-                                .bigText("Much longer text that cannot fit one line..."))
-                .setContentTitle("Mirea");
-        int importance = NotificationManager.IMPORTANCE_DEFAULT;
-
-        NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "Student KashinaAS Notification", importance);
-
-                channel.setDescription("MIREA Channel");
-
-        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
-
-        notificationManager.createNotificationChannel(channel);
-        notificationManager.notify(1, builder.build());
+            ActivityCompat.requestPermissions(this, new String[]{POST_NOTIFICATIONS}, PermissionCode);
+        }
     }
-
+    public void  onClickSendNotification(View view){
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID, "My Notifications", NotificationManager.IMPORTANCE_DEFAULT);
+            notificationChannel.setDescription("Channel description");
+            notificationChannel.enableLights(true);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+        NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(this, CHANNEL_ID)
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setContentTitle("Congratulation!")
+                        .setContentText("Notification text for MIREA")
+                        .setProgress(100, 50, false);
+        Notification notification = builder.build();
+        notificationManager.notify(IDENTIFICATE_MSG++, notification);
+    }
 }
