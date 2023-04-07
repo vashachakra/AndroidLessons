890a54c3d6e4f7d8a29851cf0b5e6c9b4ac07426
vashachakra <hey_just_bee@mail.ru>
2023-04-07 09:14:41 +0300

    start sharer

---
+++ Lesson3/sharer/src/main/java/ru/mirea/kashina/sharer/SendActivity.java
@@ -0,0 +1,22 @@
+package ru.mirea.kashina.sharer;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+public class SendActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send);
+    }
+}
