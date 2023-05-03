2b096428150fe34ec14e8c8f00a6c948dfad2985
vashachakra <hey_just_bee@mail.ru>
2023-05-03 16:13:51 +0300

    start lesson6

---
+++ Lesson6/app/src/main/java/ru/mirea/kashina/lesson6/MainActivity.java
@@ -0,0 +1,13 @@
+package ru.mirea.kashina.lesson6;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
