212c16765479fb333b4162d13459cd21a4cba6ff
vashachakra <hey_just_bee@mail.ru>
2023-05-04 00:25:50 +0300

    InternalFileStorage

---
+++ Lesson6/internalfilestorage/src/main/java/ru/mirea/kashina/internalfilestorage/MainActivity.java
@@ -0,0 +1,131 @@
+package ru.mirea.kashina.internalfilestorage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    private String fileName = "mirea.txt";
+    private TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv = findViewById(R.id.textView);
+        EditText editText = findViewById(R.id.editText);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String date = editText.getText().toString();
+                String description = "Описание памятной даты";
+                writeToInternalStorage(fileName, date + " - " + description + "\n");
+            }
+        });
+
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                tv.post(new Runnable() {
+                    public void run() {
+                        tv.setText(getTextFromFile());
+                    }
+                });
+            }
+        }).start();
+    }
+
+    // открытие файла
+    public String getTextFromFile() {
+        FileInputStream fin = null;
+        try {
+            fin = openFileInput(fileName);
+            byte[] bytes = new byte[fin.available()];
+            fin.read(bytes);
+            String text = new String(bytes);
+            Log.d(LOG_TAG, text);
+            return text;
+        } catch (IOException ex) {
+            Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
+        } finally {
+            try {
+                if (fin != null)
+                    fin.close();
+            } catch (IOException ex) {
+                Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
+        return null;
+    }
+
+    /* Проверяем хранилище на доступность чтения и записи*/
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
+    /* Проверяем внешнее хранилище на доступность чтения */
+    public boolean isExternalStorageReadable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state) ||
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
+    public void writeFileToExternalStorage() {
+        File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS);
+        File file = new File(path, "FAVORITE_QUOTE.txt");
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(file.getAbsoluteFile());
+            OutputStreamWriter output = new OutputStreamWriter(fileOutputStream);
+// Запись строки в файл
+            output.write("data");
+// Закрытие потока записи
+            output.close();
+        } catch (IOException e) {
+            Log.w("ExternalStorage", "Error writing " + file, e);
+        }
+    }
+    public void writeToInternalStorage(String fileName, String text) {
+        FileOutputStream fos = null;
+        try {
+            fos = openFileOutput(fileName, MODE_PRIVATE | MODE_APPEND);
+            fos.write(text.getBytes());
+            Toast.makeText(this, "Запись в файл выполнена", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Ошибка записи в файл", Toast.LENGTH_SHORT).show();
+        } finally {
+            try {
+                if (fos != null) fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
