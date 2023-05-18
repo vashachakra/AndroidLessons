bb8d2478085c78e43b75de1cad2e3eae88752ab4
vashachakra <hey_just_bee@mail.ru>
2023-05-19 00:21:15 +0300

    finish OSMMaps

---
+++ Lesson8/osmmaps/src/main/java/ru/mirea/kashina/osmmaps/MainActivity.java
@@ -0,0 +1,156 @@
+package ru.mirea.kashina.osmmaps;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
+import androidx.preference.PreferenceManager;
+
+import android.content.Context;
+import android.location.Location;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.widget.Toast;
+
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Marker;
+import org.osmdroid.views.overlay.Polyline;
+import org.osmdroid.views.overlay.ScaleBarOverlay;
+import org.osmdroid.views.overlay.compass.CompassOverlay;
+import org.osmdroid.views.overlay.compass.InternalCompassOrientationProvider;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
+
+import java.util.ArrayList;
+
+import ru.mirea.kashina.osmmaps.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private MapView mapView = null;
+    private final String MAPKIT_API_KEY = "bf69f1aa-c134-4d5d-bc3e-2a450ef4ef02";
+
+    private ActivityMainBinding binding;
+
+    private MyLocationNewOverlay locationNewOverlay;
+    private Marker startMarker, endMarker;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Configuration.getInstance().load(getApplicationContext(),
+                PreferenceManager.getDefaultSharedPreferences(getApplicationContext()));
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        mapView = binding.mapView;
+        mapView.setZoomRounding(true);
+        mapView.setMultiTouchControls(true);
+        IMapController mapController = mapView.getController();
+        mapController.setZoom(15.0);
+        GeoPoint startPoint = new GeoPoint(55.794229, 37.700772);
+        mapController.setCenter(startPoint);
+
+        locationNewOverlay = new MyLocationNewOverlay(new
+                GpsMyLocationProvider(getApplicationContext()),mapView);
+        locationNewOverlay.enableMyLocation();
+        mapView.getOverlays().add(this.locationNewOverlay);
+
+        CompassOverlay compassOverlay = new CompassOverlay(getApplicationContext(), new
+                InternalCompassOrientationProvider(getApplicationContext()), mapView);
+        compassOverlay.enableCompass();
+        mapView.getOverlays().add(compassOverlay);
+
+        final Context context = this.getApplicationContext();
+        final DisplayMetrics dm = context.getResources().getDisplayMetrics();
+        ScaleBarOverlay scaleBarOverlay = new ScaleBarOverlay(mapView);
+        scaleBarOverlay.setCentred(true);
+        scaleBarOverlay.setScaleBarOffset(dm.widthPixels / 2, 10);
+        mapView.getOverlays().add(scaleBarOverlay);
+
+        Marker marker = new Marker(mapView);
+        marker.setPosition(new GeoPoint(55.794229, 37.700772));
+        marker.setOnMarkerClickListener(new Marker.OnMarkerClickListener() {
+            public boolean onMarkerClick(Marker marker, MapView mapView) {
+                Toast.makeText(getApplicationContext(),"MIREA!",
+                        Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+        mapView.getOverlays().add(marker);
+
+        marker.setIcon(ResourcesCompat.getDrawable(getResources(), org.osmdroid.library.R.drawable.osm_ic_follow_me_on, null));
+
+
+        Marker marker1 = new Marker(mapView);
+        marker1.setPosition(new GeoPoint(55.831388, 37.629277));
+        marker1.setOnMarkerClickListener(new Marker.OnMarkerClickListener() {
+            public boolean onMarkerClick(Marker marker1, MapView mapView) {
+                Toast.makeText(getApplicationContext(),"VDNX",
+                        Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+        mapView.getOverlays().add(marker1);
+        marker.setIcon(ResourcesCompat.getDrawable(getResources(), org.osmdroid.library.R.drawable.osm_ic_follow_me_on, null));
+        marker.setTitle("Title");
+
+        Marker marker2 = new Marker(mapView);
+        marker2.setPosition(new GeoPoint(55.862095, 37.825685));
+        marker2.setOnMarkerClickListener(new Marker.OnMarkerClickListener() {
+            public boolean onMarkerClick(Marker marker2, MapView mapView) {
+                Toast.makeText(getApplicationContext(),"Лосиный остров",
+                        Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+        mapView.getOverlays().add(marker2);
+        marker.setIcon(ResourcesCompat.getDrawable(getResources(), org.osmdroid.library.R.drawable.osm_ic_follow_me_on, null));
+        marker.setTitle("Title");
+
+        Marker marker3 = new Marker(mapView);
+        marker3.setPosition(new GeoPoint(55.729219, 37.600891));
+        marker3.setOnMarkerClickListener(new Marker.OnMarkerClickListener() {
+            public boolean onMarkerClick(Marker marker3, MapView mapView) {
+                Toast.makeText(getApplicationContext(),"Парк Горького",
+                        Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+        mapView.getOverlays().add(marker3);
+        marker.setIcon(ResourcesCompat.getDrawable(getResources(), org.osmdroid.library.R.drawable.osm_ic_follow_me_on, null));
+        marker.setTitle("Title");
+
+        Marker marker4 = new Marker(mapView);
+        marker4.setPosition(new GeoPoint(43.345068, 42.465897));
+        marker4.setOnMarkerClickListener(new Marker.OnMarkerClickListener() {
+            public boolean onMarkerClick(Marker marker4, MapView mapView) {
+                Toast.makeText(getApplicationContext(),"Эльбрус",
+                        Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+        mapView.getOverlays().add(marker4);
+        marker.setIcon(ResourcesCompat.getDrawable(getResources(), org.osmdroid.library.R.drawable.osm_ic_follow_me_on, null));
+        marker.setTitle("Title");
+
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        Configuration.getInstance().load(getApplicationContext(),
+                PreferenceManager.getDefaultSharedPreferences(getApplicationContext()));
+        if (mapView != null) {
+            mapView.onResume();
+        }
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        Configuration.getInstance().save(getApplicationContext(),
+                PreferenceManager.getDefaultSharedPreferences(getApplicationContext()));
+        if (mapView != null) {
+            mapView.onPause();
+        }
+    }
+}
